6c12e09e9e1544cb90c88f4cf245c25ce60a8b67
Kirill-Fokin <kirill19011998@mail.ru>
2024-01-21 02:04:35 +0300

    feat: add sass styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-export function burgerInit() {
+ function burgerInit() {
     const burger = document.querySelector('.header__burger-btn')
     const header = document.querySelector('.header')
     
     burger.addEventListener('click', () =>  header.classList.contains('open') ?
     header.classList.remove('open') : header.classList.add('open'));
     }  
+
+
+    window.addEventListener("resize", () => {
+      let phoneLogo = document.createElement('div');
+      let phoneNumber = document.querySelector('.header__number');
+      if (window.innerWidth < 1300) {
+        console.log(window.innerWidth)
+        phoneLogo.classList.add('phonelogo')
+        phoneNumber.innerHTML = '';
+        phoneNumber.append(phoneLogo);
+      } else {
+        phoneNumber.innerHTML = '';
+        phoneLogo = document.createElement('a');
+        phoneNumber.textContent = '+7 (3496) 45-80-00'
+        // phoneLogo.classList.add('')
+      }
+      console.log(window.innerWidth)
+    });
