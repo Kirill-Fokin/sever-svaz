2413223c13b2bb4e1ba3b5e900b3857fa9b13ac6
Kirill-Fokin <kirill19011998@mail.ru>
2024-01-22 12:42:25 +0300

    feat: add standart layout

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
- function burgerInit() {
-    const burger = document.querySelector('.header__burger-btn')
-    const header = document.querySelector('.header')
+//  function burgerInit() {
+//     const burger = document.querySelector('.header__burger-btn')
+//     const header = document.querySelector('.header')
     
-    burger.addEventListener('click', () =>  header.classList.contains('open') ?
-    header.classList.remove('open') : header.classList.add('open'));
-    }  
+//     burger.addEventListener('click', () =>  header.classList.contains('open') ?
+//     header.classList.remove('open') : header.classList.add('open'));
+//     }  
 
 
     window.addEventListener("resize", () => {
-      let phoneLogo = document.createElement('div');
-      let phoneNumber = document.querySelector('.header__number');
-      if (window.innerWidth < 1300) {
-        console.log(window.innerWidth)
-        phoneLogo.classList.add('phonelogo')
-        phoneNumber.innerHTML = '';
-        phoneNumber.append(phoneLogo);
-      } else {
-        phoneNumber.innerHTML = '';
-        phoneLogo = document.createElement('a');
-        phoneNumber.textContent = '+7 (3496) 45-80-00'
-        // phoneLogo.classList.add('')
-      }
-      console.log(window.innerWidth)
+      const 
+
+
+
+      const mainTitle = document.querySelector('.systems__title')
+
+      const percent = window.innerWidth/100
+      console.log(percent)
+      mainTitle.style.width =`${710 - percent}px`;
+      console.log( mainTitle.style.width )
     });
